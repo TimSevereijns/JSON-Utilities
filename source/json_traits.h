2817ae5bb6daf2c95a49f43d51471cd67fbe6579
Tim Severeijns <tim.severeijns@gmail.com>
2019-08-26 21:30:39 -0700

    Type Traits No Longer Depend Directly on Inclusion of Std Containers
    + Instead of including the headers for `map`, `unordered_map`, and `vector`, we'll now detect these types of containers based on the presence of a `std::pair<...>` value-type.

--- source/json_traits.h
+++ source/json_traits.h
@@ -1,11 +1,8 @@
 #pragma once
 
-#include <map>
 #include <string>
 #include <type_traits>
-#include <unordered_map>
 #include <utility>
-#include <vector>
 
 #include "future_std.h"
 
@@ -35,16 +32,44 @@ struct has_emplace<
 {
 };
 
-template <typename, typename = void> struct treat_as_array : std::false_type
+template <typename, typename = void> struct is_container : std::false_type
 {
 };
 
 template <typename Type>
-struct treat_as_array<
+struct is_container<
     Type, future_std::void_t<
-              typename Type::value_type, typename Type::reference, typename Type::const_reference,
-              typename Type::iterator, decltype(std::declval<Type&>().begin()),
-              decltype(std::declval<Type&>().end())>> : std::true_type
+              typename Type::value_type, typename Type::iterator,
+              decltype(std::declval<Type&>().begin()), decltype(std::declval<Type&>().end())>>
+    : std::true_type
+{
+};
+
+template <typename> struct is_pair : std::false_type
+{
+};
+
+template <typename FirstType, typename SecondType>
+struct is_pair<std::pair<FirstType, SecondType>> : std::true_type
+{
+};
+
+template <typename, typename = void> struct treat_as_array : std::false_type
+{
+};
+
+/**
+ * @note Uses SFINAE to detect whether the input type has a `value_type` definition, and if it does,
+ * additional type traits will determine whether we're dealing with a container whose value type is
+ * something other than a `std::pair<...>`.
+ **/
+template <typename DataType>
+struct treat_as_array<DataType, future_std::void_t<typename DataType::value_type>>
+    : std::conditional<
+          future_std::conjunction<
+              is_container<DataType>,
+              future_std::negation<is_pair<typename DataType::value_type>>>::value,
+          std::true_type, std::false_type>::type
 {
 };
 
@@ -71,22 +96,6 @@ struct treat_as_array<std::basic_string<CharacterType, CharacterTraitsType, Allo
 {
 };
 
-template <typename KeyType, typename ValueType>
-struct treat_as_array<std::map<KeyType, ValueType>> : std::false_type
-{
-};
-
-template <typename KeyType, typename ValueType>
-struct treat_as_array<std::unordered_map<KeyType, ValueType>> : std::false_type
-{
-};
-
-// @todo Generalize to handle any array-type of pairs.
-template <typename KeyType, typename ValueType>
-struct treat_as_array<std::vector<std::pair<KeyType, ValueType>>> : std::false_type
-{
-};
-
 #if __cplusplus >= 201703L // C++17
 
 template <typename DataType>
@@ -100,18 +109,15 @@ template <typename, typename = void> struct treat_as_object : std::false_type
 {
 };
 
-template <typename KeyType, typename ValueType>
-struct treat_as_object<std::map<KeyType, ValueType>> : std::true_type
-{
-};
-
-template <typename KeyType, typename ValueType>
-struct treat_as_object<std::unordered_map<KeyType, ValueType>> : std::true_type
-{
-};
-
-template <typename KeyType, typename ValueType>
-struct treat_as_object<std::vector<std::pair<KeyType, ValueType>>> : std::true_type
+/**
+ * @note Uses SFINAE to detect whether the input type has a `value_type` definition, and if it does,
+ * additional type traits will determine whether we're dealing with a container whose value type is
+ *a `std::pair<...>`.
+ **/
+template <typename DataType>
+struct treat_as_object<DataType, future_std::void_t<typename DataType::value_type>>
+    : std::conditional<
+          is_pair<typename DataType::value_type>::value, std::true_type, std::false_type>::type
 {
 };
 
